614125b5b5dd2b398bc408a6b3ada97b354df937
hisham1234 <hishamnawzer@gmail.com>
2021-01-12 22:03:40 +0900

    first

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Csharp_Intermediate
+{
+    partial class Program
+    {
+        public class Person
+        {
+            public string Name;
+            public static Person Parse(string text)
+            {
+                var person = new Person();
+                person.Name = text;
+                return person;
+            }
+
+            public void Introduce(string to)
+            {
+                Console.WriteLine("Hi {0} I am {1}",Name,to);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Intermediate
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = Person.Parse("Hisham");
+            
+            p.Introduce("Mosh");
+        }
+    }
+}
